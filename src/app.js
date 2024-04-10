bf407d4efcc7208d5faff906650dcbadeb5b12a7
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-04-10 15:40:42 +0530

    origin

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:5173",
     Credential: true,
     methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
   })
