92a9f0005f695692daa403c99c80de111d5d5f55
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-05-16 11:44:57 +0530

    cors origin set for all

--- src/app.js
+++ src/app.js
@@ -9,13 +9,7 @@ dotenv.config({
 const app = express();
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    Credential: true,
-    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
