febca2bd8602564d37e31b0fa3e782157c82d9cf
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-07-02 19:39:38 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@ dotenv.config({
 const app = express();
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "process.env.ORIGIN",
+    Credential: true,
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
