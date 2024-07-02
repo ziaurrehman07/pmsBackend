10802c52ca452292cfc15e8b0da398a169b47657
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-07-02 22:31:46 +0530

    mongodb direct without env

--- src/DB/index.js
+++ src/DB/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGOOSE_URI}`
+      "mongodb+srv://ziaurrehman939:oT5Lx0AWHmX0RO2u@cluster0.u8nw0he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log(
       `MongoDB connected successfully! DB Host :${connectionInstance.connection.host} `
